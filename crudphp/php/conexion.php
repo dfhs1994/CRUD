8238ad461b4adbd10e17d161ad745d13f0fd7ec0
FernandoRiffo <fdo.riffo93@gmail.com>
2017-06-01 20:30:23 -0400

    respuesta a mick

--- crudphp/php/conexion.php
+++ crudphp/php/conexion.php
@@ -1,7 +1,7 @@
 <?php
 $host="localhost";
 $user="root";
-$password="1234";
+$password="";
 $db="crud1";
 $con = new mysqli($host,$user,$password,$db);
 
--- crudphp/php/navbar.php
+++ crudphp/php/navbar.php
@@ -8,7 +8,7 @@
       <span class="icon-bar"></span>
       <span class="icon-bar"></span>
     </button>
-    <a class="navbar-brand" href="./"><b>Wena wena!</b></a>
+    <a class="navbar-brand" href="./"><b>Wena wena!..... wena shoro!!</b></a>
   </div>
 
   <!-- Collect the nav links, forms, and other content for toggling -->
