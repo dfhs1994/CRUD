60f0da92ef8038ae3979aaccc52b1456eac8e547
Mick <mick@MickPC>
2017-06-01 20:26:04 -0400

    Intento de Commit 3

--- crudphp/php/navbar.php
+++ crudphp/php/navbar.php
@@ -8,7 +8,7 @@
       <span class="icon-bar"></span>
       <span class="icon-bar"></span>
     </button>
-    <a class="navbar-brand" href="./"><b>CRUD PHP MYSQL</b></a>
+    <a class="navbar-brand" href="./"><b>Wena wena!</b></a>
   </div>
 
   <!-- Collect the nav links, forms, and other content for toggling -->
